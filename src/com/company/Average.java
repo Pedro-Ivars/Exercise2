450282ab20936331f644d5095e0f3fd4a588ef53
Pedro <pedroivi314@gmail.com>
2020-11-10 13:54:27 +0100

    Fix Average file

--- src/com/company/Average.java
+++ src/com/company/Average.java
@@ -1,4 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Average {
+
+    public static void Average() {
+
+        double total = 0, dataUser, counter = 0, average;
+        String s;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Insert a mark: ");
+        s = scan.nextLine();
+
+        while (!s.equals("end")) {
+            dataUser = Double.parseDouble(s);
+            counter++;
+            total += dataUser;
+            System.out.print("Insert a mark: ");
+            s = scan.nextLine();
+        }
+
+        if (counter != 0) {
+            average = total / counter;
+            System.out.println("The average is " + average);
+        }
+    }
 }
